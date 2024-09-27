9f3a74b88d42744872e6b6c76052b9e3d72af70e
zhangly <zhangly.sjtu@gmail.com>
2024-09-26 22:34:33 -0500

    Updated an example of pipeline

--- Subplot.py
+++ Subplot.py
@@ -1,39 +1,42 @@
 class Curve:
-    def __init__(self, x, y, curve_value, error_bars=None):
+    def __init__(self, color, shape, x, y, curve_value, error_bars=None):
+        # Must contain the color and shape of the curve to distinguish it from other curves
+        self.color = color
+        self.shape = shape
         self.x = x
         self.y = y
         self.error_bars = error_bars
         self.curve_value = curve_value
 
 class DotPlot:
-    def __init__(self, x, y, subplot_value):
+    def __init__(self, x, y, subplot_value, has_error_bars):
+        # This initialization will be called the LLM
         self.x_axis = x
         self.y_axis = y
         self.subplot_value = subplot_value
+        self.has_error_bars = has_error_bars
 
-    def estimate(self, has_error_bars):
-        curves = []
-        for subplot in self.subplots:
-            curves.append(subplot.estimate_dot_points(has_error_bars))
+    def estimate(self, image):
+        pass
 
 class Histogram:
-    def __init__(self, x, y, subplot_value):
+    def __init__(self, x, y, subplot_value, has_error_bars):
+        # This initialization will be called the LLM
         self.x_axis = x
         self.y_axis = y
         self.subplot_value = subplot_value
+        self.has_error_bars = has_error_bars
 
-    def estimate(self, has_error_bars):
-        curves = []
-        for subplot in self.subplots:
-            curves.append(subplot.estimate_histogram(has_error_bars))
+    def estimate(self, image):
+        pass
 
 class Continuous:
-    def __init__(self, x, y, subplot_value):
+    def __init__(self, x, y, subplot_value, has_error_bars):
+        # This initialization will be called the LLM
         self.x_axis = x
         self.y_axis = y
         self.subplot_value = subplot_value
+        self.has_error_bars = has_error_bars
 
-    def estimate(self, has_error_bars):
-        curves = []
-        for subplot in self.subplots:
-            curves.append(subplot.estimate_continuous(has_error_bars))
+    def estimate(self, image):
+        pass
