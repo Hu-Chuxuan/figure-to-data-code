50e1da4de1d0325434c50d3c721f74e26d6b0cc8
zhangly <zhangly.sjtu@gmail.com>
2024-10-27 23:56:59 -0500

    Track more statistics. Fixed some bugs

--- PlotEvaluator.py
+++ PlotEvaluator.py
@@ -25,20 +25,32 @@ def process(value):
     return value
 
 class Curve:
-
     def __setattr__(self, name: str, value: list) -> None:
         if value is None or len(value) == 0:
             super().__setattr__(name, None)
             return
+        parsed_values = []
+        any_str = False
         for i in range(len(value)):
-            value[i] = process(value[i])
-        if type(value[0]) != str:
-            array = np.array(value, dtype=np.float64)
+            parsed_values.append(process(value[i]))
+            if type(parsed_values[-1]) != type(parsed_values[0]):
+                any_str = True
+                break
+        if not any_str:
+            array = np.array(parsed_values, dtype=np.float64)
             super().__setattr__(name, array)
         else:
-            super().__setattr__(name, value)
+            array = []
+            for value in parsed_values:
+                if isinstance(value, np.ndarray):
+                    assert value.shape == (2,)
+                    str_val = f"{value[0]}-{value[1]}"
+                else:
+                    str_val = str(value)
+                array.append(str_val)
+            super().__setattr__(name, array)
 
-    def __getattribute__(self, name: str) -> np.ndarray:
+    def __getattribute__(self, name: str):
         try:
             return super().__getattribute__(name)
         except AttributeError:
@@ -54,15 +66,25 @@ class Curve:
             setattr(self, key, value)
 
     def select(self, indices):
+        if indices is None:
+            self.x = None
+            self.y = None
+            self.err = None
         if self.x is not None:
-            if type(self.x) == np.ndarray:
+            if type(self.x) == np.ndarray and self.x.dtype != object:
                 self.x = self.x[indices]
             else:
                 self.x = None
         if self.y is not None:
-            self.y = self.y[indices]
+            if type(self.y) == np.ndarray and self.y.dtype != object:
+                self.y = self.y[indices]
+            else:
+                self.y = None
         if self.err is not None:
-            self.err = self.err[indices]
+            if type(self.err) == np.ndarray and self.err.dtype != object:
+                self.err = self.err[indices]
+            else:
+                self.err = None
     
     def getter(self, attributes):
         res = []
@@ -97,6 +119,8 @@ def is_empty(value):
 '''
 def pair_score(pred, gt):
     if type(gt) == str:
+        if type(pred) == np.ndarray:
+            return np.inf
         return 0 if gt == pred else np.inf
     if is_empty(pred) and is_empty(gt):
         return 0
@@ -119,7 +143,11 @@ def pair_data_points(pred_curve: Curve, gt_curve: Curve):
     for i in range(len(pred_curve)):
         for j in range(len(gt_curve)):
             cost_matrix[i, j] = pair_score(pred_curve.x[i], gt_curve.x[j])
-    row_idx, col_idx = linear_sum_assignment(cost_matrix)
+    try:
+        row_idx, col_idx = linear_sum_assignment(cost_matrix)
+    except:
+        row_idx = None
+        col_idx = None
     pred_curve.select(row_idx)
     gt_curve.select(col_idx)
 
@@ -143,8 +171,12 @@ def separate_curve(df):
     for i in range(len(df)):
         if "Subplot Value" in df.columns and cur_subplot != df["Subplot Value"][i]:
             cur_subplot = df["Subplot Value"][i]
+            if isinstance(cur_subplot, float) and np.isnan(cur_subplot):
+                cur_subplot = None
         if "Type-2" in df.columns and cur_type2 != df["Type-2"][i]:
             cur_type2 = df["Type-2"][i]
+            if isinstance(cur_type2, float) and np.isnan(cur_type2):
+                cur_type2 = None
         if (cur_subplot, cur_type2) not in curves:
             curves[(cur_subplot, cur_type2)] = {"x": [], "y": [], "err": []}
         curves[(cur_subplot, cur_type2)]["x"].append(df["Type-1"][i])
@@ -163,6 +195,7 @@ def separate_curve(df):
 def pair_curves(pred_curves, gt_curves):
     paired_curves = {}
     paired = {}
+    # pair_trace = ""
     for subplot_gt, type2_gt in gt_curves.keys():
         best_score = np.inf
         best_subplot_pred, best_type2_pred = None, None
@@ -179,19 +212,24 @@ def pair_curves(pred_curves, gt_curves):
         if best_score < np.inf:
             paired[(best_subplot_pred, best_type2_pred)] = (subplot_gt, type2_gt)
             pred_curve = pred_curves[(best_subplot_pred, best_type2_pred)]
+            # pair_trace += f"Pairing ({subplot_gt}, {best_subplot_pred}) and ({type2_gt}, {best_type2_pred})\n"
         else:
             pred_curve = {}
+            # pair_trace += f"Curve ({subplot_gt}, {type2_gt}) cannot be paired with any curve in prediction\n"
         if subplot_gt not in paired_curves:
             paired_curves[subplot_gt] = []
         gt_curve = gt_curves[(subplot_gt, type2_gt)]
         paired_curves[subplot_gt].append((Curve(pred_curve), Curve(gt_curve)))
     
+    # pair_trace += "Unpaired curves in prediction:\n"
     for subplot_pred, type2_pred in pred_curves.keys():
         if (subplot_pred, type2_pred) not in paired:
             pred_curve = pred_curves[(subplot_pred, type2_pred)]
             if subplot_pred not in paired_curves:
                 paired_curves[subplot_pred] = []
             paired_curves[subplot_pred].append((Curve(pred_curve), Curve({})))
+            # pair_trace += f"({subplot_pred}, {type2_pred}), "
+    # pair_trace += "\n"
 
     return paired_curves
 
@@ -240,14 +278,14 @@ def cal_perf(curves_in_subplot):
                     )
             if "gt_len" in curve:
                 if curve["gt_len"] == 0:
-                    perf["Identified rate"] = 0
+                    perf["DP accuracy"] = 0
                 else:
-                    perf["Identified rate"] = len(curve["gt"]) / curve["gt_len"]
+                    perf["DP accuracy"] = min(len(curve["gt"]), len(curve["pred"])) / curve["gt_len"]
             if "pred_len" in curve:
                 if curve["pred_len"] == 0:
-                    perf["Identified recall"] = 0
+                    perf["DP recall"] = 0
                 else:
-                    perf["Identified recall"] = len(curve["gt"]) / curve["pred_len"]
+                    perf["DP recall"] = min(len(curve["gt"]), len(curve["pred"])) / curve["pred_len"]
             curve_level_perf[subplot].append(perf)
     subplot_level_perf = []
     for subplot, curve_perf in curve_level_perf.items():
@@ -298,12 +336,23 @@ def evaluate_plot(pred_df, gt_df):
         paired_curves = pair_curves(gt_curves, pred_curves)
         pred_idx, gt_idx = 1, 0
 
+    if len(paired_curves) == 0:
+        perf = {"DP accuracy": 0, "DP recall": 0, "Curve accuracy": 0, "Curve recall": 0}
+        return perf
+
+    paired_curve_num = 0
+    for curves in paired_curves.values():
+        for curve in curves:
+            if len(curve[0]) != 0 and len(curve[1]) != 0:
+                paired_curve_num += 1
+
     curves_in_subplot = {}
-    
     for subplot_gt, curve_pairs in paired_curves.items():
         curves = []
         for curve_pair in curve_pairs:
             pred_curve, gt_curve = curve_pair[pred_idx], curve_pair[gt_idx]
+            if len(pred_curve) == 0 or len(gt_curve) == 0:
+                continue
             if len(gt_curve) >= 50:
                 # This is a continuous plot
                 x_common = np.linspace(
@@ -325,5 +374,10 @@ def evaluate_plot(pred_df, gt_df):
                     "gt_len": gt_len, "pred_len": pred_len
                 })
         curves_in_subplot[subplot_gt] = curves
-
-    return cal_perf(curves_in_subplot)
+    perf = cal_perf(curves_in_subplot)
+    perf["Curve accuracy"] = paired_curve_num / len(gt_curves)
+    if len(pred_curves) == 0:
+        perf["Curve recall"] = 0
+    else:
+        perf["Curve recall"] = paired_curve_num / len(pred_curves)
+    return perf
