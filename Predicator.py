49dd4d9e5ffeca4f98afc951ee77f84ed16b4315
zhangly <zhangly.sjtu@gmail.com>
2024-10-28 11:33:45 -0500

    Tracked more statistics

--- Predicator.py
+++ Predicator.py
@@ -3,23 +3,22 @@ import cv2
 import argparse
 import pandas as pd
 import logging
+import json
 
 from Baseline.gpt import digitize
-from Evaluator import evaluate_plot, evaluate_table, WrongCSVNumberError, FormatError
-
-parser = argparse.ArgumentParser(description='Digitize a plot or a table from an image')
-parser.add_argument('--root', type=str, help='Path to the image file')
-parser.add_argument("--output", type=str, help="Path to the output CSV file")
-parser.add_argument('--api', type=str, help='OpenAI API key')
-parser.add_argument('--org', type=str, help='OpenAI organization')
-args = parser.parse_args()
+from Baseline.baseline import baseline_prompt
+from PlotEvaluator import evaluate_plot, merge_perf, WrongCSVNumberError, FormatError
+from TableEvaluator import evaluate_table
 
 MAX_RETRIES = 1
 RED = '\033[91m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
 RESET = '\033[0m'
 
 class Dataset:
-    def __init__(self, root, types, paper_list=None):
+    def __init__(self, root, types, paper_list=None, read_txt=False):
+        self.read_txt = read_txt
         self.samples = []
         if paper_list is None:
             paper_list = os.listdir(root)
@@ -29,51 +28,154 @@ class Dataset:
                 if sample[0] in types and (sample.endswith(".png") or sample.endswith(".jpeg")):
                     name = sample[:sample.rfind(".")]
                     if name[0] == "T" or name+".csv" in samples:
-                        self.samples.append((os.path.join(root, paper, sample), name))
+                        self.samples.append((os.path.join(root, paper, sample), paper, name))
     
     def __len__(self):
         return len(self.samples)
     
     def __getitem__(self, idx):
-        path, name = self.samples[idx]
+        path, paper, name = self.samples[idx]
+        sample_idx = name[name.find("-O")+2:]
         img = cv2.imread(path)
         gts = []
         path = path[:path.rfind("/")]
         files = sorted(os.listdir(path))
         for file in files:
-            if name in file and file.endswith(".csv"):
-                gts.append(pd.read_csv(os.path.join(path, file)))
-        return img, name, gts
+            if not file.endswith(".csv"):
+                continue
+            file_idx = file[file.find("-O")+2:file.rfind(".")]
+            if "-" in file_idx:
+                file_idx = file_idx[:file_idx.find("-")]
+            if file_idx == sample_idx and name in file:
+                if self.read_txt:
+                    with open(os.path.join(path, file), "r") as f:
+                        gts.append(f.read())
+                else:
+                    gts.append(pd.read_csv(os.path.join(path, file)))
+        return img, paper, name, gts
 
-dataset = Dataset(args.root, ["P", "T"])
-for i in range(len(dataset)):
-    img, file_name, gts = dataset[i]
-    perf = None
-    print("===================", file_name, "===================")
-    for _ in range(MAX_RETRIES):
-        res, response = digitize(img, args.api, args.org)
-        with open(os.path.join(args.output, file_name+".txt"), "w") as f:
-            f.write(response)
-        
-        if res == None:
-            continue
-        for i, df in enumerate(res):
-            if len(res) == 1:
-                df.to_csv(os.path.join(args.output, file_name+".csv"), index=False)
+
+def main(args):
+    train_paper = ["2", "10", "14", "16", "20", "47", "49", "74", "86", "92", "100"]
+    valid_paper = ["4", "6", "18", "24", "36", "48", "56", "62", "88", "94", "98"]
+    dataset = Dataset(args.root, ["T"], valid_paper)
+
+    total_csv = [0, 0]
+    valid_csv = [0, 0]
+    invalid_format = [0, 0]
+    invalid_num = [0, 0]
+    invalid_col = [0, 0]
+    perf_per_sample = {}
+
+    for i in range(len(dataset)):
+        img, paper, file_name, gts = dataset[i]
+        if not os.path.exists(os.path.join(args.output, str(paper))):
+            os.makedirs(os.path.join(args.output, str(paper)))
+        perf = None
+        print("===================", file_name, "===================")
+        if file_name[0] == "P":
+            idx = 0
+        else:
+            idx = 1
+        total_csv[idx] += 1
+        for retry in range(MAX_RETRIES):
+            print("***", retry+1, "trial ***")
+            try:
+                res, response = digitize(baseline_prompt, img, args.api, args.org, args.model)
+                with open(os.path.join(args.output, str(paper), file_name+".txt"), "w") as f:
+                    f.write(response)
+            except pd.errors.ParserError as e:
+                print(e)
+                invalid_format[idx] += 1
+                continue
+            except Exception as e:
+                logging.error(RED + str(e) + RESET)
+                input("Press Enter to continue...")
+            
+            if res == None:
+                continue
+            for i, df in enumerate(res):
+                if len(res) == 1:
+                    df.to_csv(os.path.join(args.output, str(paper), file_name+".csv"), index=False)
+                else:
+                    df.to_csv(os.path.join(args.output, str(paper), f"{file_name}-{i}.csv"), index=False)
+            
+            try:
+                if file_name[0] == "P":
+                    perf, pair_trace = evaluate_plot(res, gts)
+                    print(GREEN + pair_trace + RESET)
+                else:
+                    perf, pair_trace = evaluate_table(res, gts)
+                valid_csv[idx] += 1
+                break
+            except (WrongCSVNumberError, FormatError) as e:
+                if type(e) == WrongCSVNumberError:
+                    invalid_num[idx] += 1
+                else:
+                    invalid_col[idx] += 1
+                logging.warning(e)
+                continue
+            except Exception as e:
+                logging.error(RED + str(e) + RESET)
+                input("Press Enter to continue...")
+                continue
+        if perf is not None:
+            if paper not in perf_per_sample:
+                perf_per_sample[paper] = {}
+            perf["Success retry"] = retry+1
+            perf_per_sample[paper][file_name] = perf
+        print(file_name, "performance:", perf)
+    final_perfs = {}
+    perfs_per_paper = {}
+    plot_perfs, table_perfs = [], []
+    for paper in perf_per_sample:
+        plots, tables = [], []
+        for key in perf_per_sample[paper]:
+            if key[0] == "P":
+                plots.append(perf_per_sample[paper][key])
             else:
-                df.to_csv(os.path.join(args.output, f"{file_name}-{i}.csv"), index=False)
-        
-        try:
-            if file_name[0] == "P":
-                perf = evaluate_plot(res, gts)
-            else:
-                perf = evaluate_table(res, gts)
-            break
-        except (WrongCSVNumberError, FormatError) as e:
-            logging.warning(e)
-            continue
-        except Exception as e:
-            logging.error(RED + str(e) + RESET)
-            continue
-    print(file_name, "performance:", perf)
-    input("Press Enter to continue...")
+                tables.append(perf_per_sample[paper][key])
+        perfs_per_paper[paper] = {"plots": merge_perf(plots), "tables": merge_perf(tables)}
+        plot_perfs.append(perfs_per_paper[paper]["plots"])
+        table_perfs.append(perfs_per_paper[paper]["tables"])
+    final_perfs["plots"] = merge_perf(plot_perfs)
+    final_perfs["tables"] = merge_perf(table_perfs)
+
+    print("====================================\n")
+    if total_csv[0] > 0:
+        final_perfs["plots"]["Total"] = total_csv[0]
+        final_perfs["plots"]["Valid"] = valid_csv[0]
+        final_perfs["plots"]["Invalid CSV format"] = invalid_format[0]
+        final_perfs["plots"]["Invalid CSV number"] = invalid_num[0]
+        final_perfs["plots"]["Invalid column"] = invalid_col[0]
+        print(f"Plot: total {total_csv[0]}, valid {valid_csv[0]}, invalid format {invalid_format[0]}, invalid number {invalid_num[0]}, invalid column {invalid_col[0]}")
+        print("Final performance:", )
+        for key, value in final_perfs["plots"].items():
+            print("\t", key, ":", value)
+    if total_csv[1] > 0:
+        final_perfs["tables"]["Total"] = total_csv[1]
+        final_perfs["tables"]["Valid"] = valid_csv[1]
+        final_perfs["tables"]["Invalid CSV format"] = invalid_format[1]
+        final_perfs["tables"]["Invalid CSV number"] = invalid_num[1]
+        final_perfs["tables"]["Invalid column"] = invalid_col[1]
+        print(f"Table: total {total_csv[1]}, valid {valid_csv[1]}, invalid format {invalid_format[1]}, invalid number {invalid_num[1]}, invalid column {invalid_col[1]}")
+        print("Final performance:", final_perfs["tables"])
+    print("\n====================================")
+
+    with open(os.path.join(args.output, "perfs.json"), "w") as f:
+        json.dump(final_perfs, f, indent=4)
+    with open(os.path.join(args.output, "perfs_per_paper.json"), "w") as f:
+        json.dump(perfs_per_paper, f, indent=4)
+    with open(os.path.join(args.output, "perf_per_sample.json"), "w") as f:
+        json.dump(perf_per_sample, f, indent=4)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Digitize a plot or a table from an image')
+    parser.add_argument('--root', type=str, help='Path to the image file')
+    parser.add_argument("--output", type=str, help="Path to the output CSV file")
+    parser.add_argument('--api', type=str, help='OpenAI API key')
+    parser.add_argument('--org', type=str, help='OpenAI organization')
+    parser.add_argument('--model', type=str)
+    args = parser.parse_args()
+
+    main(args)
