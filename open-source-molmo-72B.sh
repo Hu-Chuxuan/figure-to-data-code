d15f87f411dc0e71713a1c4e6fe1f9aeec752ed8
Hu-Chuxuan <chuxuan0528@gmail.com>
2024-11-03 15:07:57 -0600

    deployment script

---
+++ open-source-molmo-72B.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --job-name=amazon-0
+#SBATCH --mail-user=chuxuan3@illinois.edu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --partition=ddkang
+#SBATCH --ntasks-per-node=3
+#SBATCH --gres=gpu:H100:1
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=200G
+
+mkdir ../output/Molmo-72B
+python Predictor.py --root ../figure-to-data --output ../output/Molmo-72B --model allenai/Molmo-72B-0924
