18b5f841a61b29dade947aa3557132ed1ddfb432
Hu-Chuxuan <chuxuan0528@gmail.com>
2024-11-04 00:22:21 -0600

    qwen 7B

---
+++ open-source-qwen-7B.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --job-name=qwen-7b
+#SBATCH --mail-user=chuxuan3@illinois.edu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --partition=ddkang
+#SBATCH --ntasks-per-node=3
+#SBATCH --gres=gpu:H100:1
+#SBATCH --nodelist=ccc0419
+#SBATCH --time=10:00:00
+#SBATCH --mem=50G
+
+echo "Checking GPU availability..."
+nvidia-smi
+echo "CUDA availability in PyTorch:"
+python -c "import torch; print('CUDA available:', torch.cuda.is_available())"
+
+mkdir ../output/Qwen-7B
+python Predictor.py --root ../figure-to-data --output ../output/Qwen-7B --model Qwen/Qwen2-VL-7B-Instruct
