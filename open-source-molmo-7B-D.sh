3ed2833e4e75df6e8a6796e6ec003e5fa8ac6132
Hu-Chuxuan <chuxuan0528@gmail.com>
2024-11-03 23:16:13 -0600

    try 7b

---
+++ open-source-molmo-7B-D.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --job-name=molmo-7b-d
+#SBATCH --mail-user=chuxuan3@illinois.edu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --partition=ddkang
+#SBATCH --ntasks-per-node=3
+#SBATCH --gres=gpu:H100:1
+#SBATCH --nodelist=ccc0419
+#SBATCH --time=00:50:00
+#SBATCH --mem=200G
+
+echo "Checking GPU availability..."
+nvidia-smi
+echo "CUDA availability in PyTorch:"
+python -c "import torch; print('CUDA available:', torch.cuda.is_available())"
+
+mkdir ../output/Molmo-7B-D
+python Predictor.py --root ../figure-to-data --output ../output/Molmo-72B-D --model allenai/Molmo-7B-D-0924 --paper_list 1 2 3
