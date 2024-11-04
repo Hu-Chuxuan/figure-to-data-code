f074afb48c778c3197e4786ac02b042d4024323d
Hu-Chuxuan <chuxuan0528@gmail.com>
2024-11-04 15:48:06 -0600

    debug qwen and llava 11

--- open-source-qwen-7B.sh
+++ open-source-qwen-7B.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=qwen-7b
 #SBATCH --mail-user=chuxuan3@illinois.edu
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --partition=ddkang
+#SBATCH --partition=secondary
 #SBATCH --ntasks-per-node=3
 #SBATCH --gres=gpu:H100:1
 #SBATCH --time=00:20:00
