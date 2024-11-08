85fdc37397496c14cd39cef152f46f3f63fd4bd2
Hu-Chuxuan <chuxuan0528@gmail.com>
2024-11-07 22:59:22 -0600

    internvl76b debug4

--- Baseline/mllm.py
+++ Baseline/mllm.py
@@ -377,6 +377,7 @@ class InternVL:
 
     def query(self, prompt, image_path):
         pixel_values = load_image(image_path, max_num=12).to(torch.bfloat16).cuda()
+        logging.warning(f"Pixel values tensor device: {pixel_values.device}")
         generation_config = dict(max_new_tokens=1024, do_sample=True)
 
         response = self.model.chat(self.tokenizer, pixel_values, '<image>\n'+prompt, generation_config)
