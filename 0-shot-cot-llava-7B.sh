236cee73a8c38c74e4e8d80b06f246174bda556b
Hu-Chuxuan <chuxuan0528@gmail.com>
2024-11-18 01:22:20 -0600

    llava cont

--- 0-shot-cot-llava-7B.sh
+++ 0-shot-cot-llava-7B.sh
@@ -15,4 +15,4 @@ echo "CUDA availability in PyTorch:"
 python -c "import torch; print('CUDA available:', torch.cuda.is_available())"
 
 mkdir ../output/Llava-7B-0-Shot-COT
-python Predictor.py --root ../figure-to-data --output ../output/Llava-7B-0-Shot-COT --model lmms-lab/llava-onevision-qwen2-7b-ov --Prompt cot --shot 0
+python Predictor.py --root ../figure-to-data --output ../output/Llava-7B-0-Shot-COT --model lmms-lab/llava-onevision-qwen2-7b-ov --Prompt cot --shot 0 --types Table --paper_list 95 96 97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122
