1e3d7de83a661fc5490bafe560f498501ac45015
Hu-Chuxuan <chuxuan0528@gmail.com>
2024-11-18 13:12:17 -0600

    qwen cont

--- 1-shot-cot-qwen-7B.sh
+++ 1-shot-cot-qwen-7B.sh
@@ -15,4 +15,4 @@ echo "CUDA availability in PyTorch:"
 python -c "import torch; print('CUDA available:', torch.cuda.is_available())"
 
 mkdir ../output/Qwen-7B-1-Shot-COT
-python Predictor.py --root ../figure-to-data --output ../output/Qwen-7B-1-Shot-COT --model Qwen/Qwen2-VL-7B-Instruct --Prompt cot --shot 1
+python Predictor.py --root ../figure-to-data --output ../output/Qwen-7B-1-Shot-COT --model Qwen/Qwen2-VL-7B-Instruct --Prompt cot --shot 1 --types Table --paper_list 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122
