22ec3b9f5cf495c6bf641bcbeb9778af0ec76729
Hu-Chuxuan <chuxuan0528@gmail.com>
2024-11-20 01:47:48 -0600

    molmo72b cont

--- 0-shot-cot-molmo-72B.sh
+++ 0-shot-cot-molmo-72B.sh
@@ -15,4 +15,4 @@ echo "CUDA availability in PyTorch:"
 python -c "import torch; print('CUDA available:', torch.cuda.is_available())"
 
 mkdir ../output/Molmo-72B-0-Shot-COT
-python Predictor.py --root ../figure-to-data --output ../output/Molmo-72B-0-Shot-COT --model allenai/Molmo-72B-0924 --Prompt cot --shot 0 --types Table --paper_list 94 95 96 97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122
+python Predictor.py --root ../figure-to-data --output ../output/Molmo-72B-0-Shot-COT --model allenai/Molmo-72B-0924 --Prompt cot --shot 0 --types Table --paper_list 113 114 115 116 117 118 119 120 121 122
